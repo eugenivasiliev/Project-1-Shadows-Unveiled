192b632c48a3a3d9fe8e99fab153109a6fb76529
eugenivasiliev <159423029+eugenivasiliev@users.noreply.github.com>
2024-06-25 18:12:01 +0200

    Create Checkpoint.cs

---
+++ Assets/Checkpoint.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    [SerializeField] private RespawnPoint[] checkpoints;
+    private int currentCheckpoint = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < checkpoints.Length; i++) checkpoints[i].Index = i;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetCheckpoint(int newCheckpoint)
+    {
+        currentCheckpoint = newCheckpoint;
+    }
+
+    public Vector2 GetRespawnPoint()
+    {
+        return checkpoints[currentCheckpoint].transform.position;
+    }
+}
